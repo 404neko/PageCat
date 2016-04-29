77f4fdbf255d2c4aa8d2f8cd19656456ea6a4507
Alex <404neko@gmail.com>
2016-04-29 21:30:52 +0800

    new mail 5

--- notice/notice.py
+++ notice/notice.py
@@ -33,81 +33,84 @@ running_tasks = {}
 
 if __name__ == '__main__':
     while True:
-        database.connect()
-        users_ = User.select()
-        users = []
-        for user in users_:
-            if user.sideload not in ['(NULL)',None,'None','null']:
-                users.append(user)
+        try:
+            database.connect()
+            users_ = User.select()
+            users = []
+            for user in users_:
+                if user.sideload not in ['(NULL)',None,'None','null']:
+                    users.append(user)
+                else:
+                    pass
+            if len(users) == 0:
+                time.sleep(SCAN_TASKLIST)
+                database.close()
+                continue
             else:
-                pass
-        if len(users) == 0:
-            time.sleep(SCAN_TASKLIST)
-            database.close()
-            continue
-        else:
-            for user in users:
-                now_tasks.append(user.id)
-                if user.id not in running_tasks:
-                    def task_fun(user_id,user_mail,sideload):
-                        database.connect()
-                        Log('Mail: '+user_mail)
-                        tid = user_id
-                        tasks = json.loads(sideload)['tasks']
-                        changed_tasks = {}
-                        for task_id in tasks:
+                for user in users:
+                    now_tasks.append(user.id)
+                    if user.id not in running_tasks:
+                        def task_fun(user_id,user_mail,sideload):
                             database.connect()
-                            url_ = Task.select().where(Task.id==task_id)
-                            if len(url_)!=0:
-                                url_ = url_[0]
-                                url_ = url_.url
-                            else:
-                                url_ = ''
-                            items = Pool.select().where(Pool.tid==task_id).limit(2).order_by(Pool.time.desc())
-                            if len(items)==2:
-                                content_0 = get_text(items[0].data)
-                                content_1 = get_text(items[1].data)
-                                #print type(content_0), content_0
-                                if similarity(content_0,content_1)<SIMILARITY:
-                                    old_md = html2text.html2text(items[0].data)
-                                    new_md = html2text.html2text(items[1].data)
-                                    l,r = strom(old_md,new_md)
-                                    text = strom_mail(l,r)
-                                    changed_tasks[task_id] = text
+                            Log('Mail: '+user_mail)
+                            tid = user_id
+                            tasks = json.loads(sideload)['tasks']
+                            changed_tasks = {}
+                            for task_id in tasks:
+                                database.connect()
+                                url_ = Task.select().where(Task.id==task_id)
+                                if len(url_)!=0:
+                                    url_ = url_[0]
+                                    url_ = url_.url
                                 else:
-                                    pass
-                        #print changed_tasks
-                        changed_task_urls = {}
-                        if changed_tasks=={}:
-                            Log('Mail: '+user_mail+',no changes.')
-                            #print 'in if'
-                            #return 0
-                        else:
-                            for task_id in changed_tasks:
-                                url = Task.select().where(Task.id==task_id)[0].url
-                                changed_task_urls[url] = changed_tasks[task_id]
-                        text = 'When you are not in:\n\n'
-                        for url in changed_task_urls:
-                            text+='    '
-                            text+=url
-                            text+=':'
-                            text+=markdown2text(changed_task_urls[url],url_)
+                                    url_ = ''
+                                items = Pool.select().where(Pool.tid==task_id).limit(2).order_by(Pool.time.desc())
+                                if len(items)==2:
+                                    content_0 = get_text(items[0].data)
+                                    content_1 = get_text(items[1].data)
+                                    #print type(content_0), content_0
+                                    if similarity(content_0,content_1)<SIMILARITY:
+                                        old_md = html2text.html2text(items[0].data)
+                                        new_md = html2text.html2text(items[1].data)
+                                        l,r = strom(old_md,new_md)
+                                        text = strom_mail(l,r)
+                                        changed_tasks[task_id] = text
+                                    else:
+                                        pass
+                            #print changed_tasks
+                            changed_task_urls = {}
+                            if changed_tasks=={}:
+                                Log('Mail: '+user_mail+',no changes.')
+                                #print 'in if'
+                                #return 0
+                            else:
+                                for task_id in changed_tasks:
+                                    url = Task.select().where(Task.id==task_id)[0].url
+                                    changed_task_urls[url] = changed_tasks[task_id]
+                            text = 'When you are not in:\n\n'
+                            for url in changed_task_urls:
+                                text+='    '
+                                text+=url
+                                text+=':'
+                                text+=markdown2text(changed_task_urls[url],url_)
+                                text+='\n'
                             text+='\n'
-                        text+='\n'
-                        text+='changed.'
-                        try:
-                            send_mail(user_mail,'Today\'s change.',text)
-                            Log('Mail: '+user_mail+' END')
-                        except:
-                            Log('Mail: '+user_mail+' FAIL')
-                        database.close()
-                        return 0
-                    every = json.loads(user.sideload).get('every',86400)
-                    running_tasks[user.id]=timer.AsyncTask(task_fun,(user.id,user.mail,user.sideload),delay_cal(every),)
-                    running_tasks[user.id].run()
-            for tid in running_tasks:
-                if tid not in now_tasks:
-                    task_ = running_tasks.remove(tid)
-                    task_.stop()
-        database.close()
-        time.sleep(SCAN_TASKLIST)
+                            text+='changed.'
+                            try:
+                                send_mail(user_mail,'Today\'s change.',text)
+                                Log('Mail: '+user_mail+' END')
+                            except:
+                                Log('Mail: '+user_mail+' FAIL')
+                            database.close()
+                            return 0
+                        every = json.loads(user.sideload).get('every',86400)
+                        running_tasks[user.id]=timer.AsyncTask(task_fun,(user.id,user.mail,user.sideload),delay_cal(every),)
+                        running_tasks[user.id].run()
+                for tid in running_tasks:
+                    if tid not in now_tasks:
+                        task_ = running_tasks.remove(tid)
+                        task_.stop()
+            database.close()
+            time.sleep(SCAN_TASKLIST)
+        except KeyboardInterrupt:
+            exit(0)
